401e02ba7b53113d3bbb08166676648d8e1572f4
Alexthw46 <77152778+Alexthw46@users.noreply.github.com>
2023-10-07 09:29:42 +0200

    FakePlayer for Command Ritual

--- src/main/java/elucent/eidolon/common/ritual/ExecCommandRitual.java
+++ src/main/java/elucent/eidolon/common/ritual/ExecCommandRitual.java
@@ -1,6 +1,7 @@
 package elucent.eidolon.common.ritual;
 
 import elucent.eidolon.api.ritual.Ritual;
+import elucent.eidolon.util.EidolonFakePlayer;
 import net.minecraft.CrashReport;
 import net.minecraft.CrashReportCategory;
 import net.minecraft.ReportedException;
@@ -9,9 +10,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.StringUtil;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.AABB;
 
 public class ExecCommandRitual extends Ritual {
 
@@ -34,16 +33,16 @@ public class ExecCommandRitual extends Ritual {
     @Override
     public RitualResult start(Level world, BlockPos pos) {
 
-        if (world instanceof ServerLevel) {
-
+        if (world instanceof ServerLevel serverWorld) {
 
             var server = world.getServer();
             if (server.isCommandBlockEnabled() && !StringUtil.isNullOrEmpty(this.command)) {
                 try {
-                    world.getEntitiesOfClass(Entity.class, new AABB(pos).inflate(10)).stream().findFirst().ifPresent(commandSender -> {
-                        CommandSourceStack commandSource = commandSender.createCommandSourceStack().withPermission(2).withSuppressedOutput();
-                        server.getCommands().performPrefixedCommand(commandSource, this.command);
-                    });
+
+                    var fakePlayer = EidolonFakePlayer.getPlayer(serverWorld);
+                    CommandSourceStack commandSource = fakePlayer.createCommandSourceStack().withPermission(2).withSuppressedOutput();
+                    server.getCommands().performPrefixedCommand(commandSource, this.command);
+
                 } catch (Throwable throwable) {
                     CrashReport crashreport = CrashReport.forThrowable(throwable, "Executing command ritual");
                     CrashReportCategory crashreportcategory = crashreport.addCategory("Command to be executed");
