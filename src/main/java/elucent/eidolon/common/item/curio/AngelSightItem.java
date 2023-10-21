1ce69681bbf839ee906026a94c647f731e6ae250
Alexthw46 <77152778+Alexthw46@users.noreply.github.com>
2023-10-22 00:22:37 +0200

    angel sight ring modes

--- src/main/java/elucent/eidolon/common/item/curio/AngelSightItem.java
+++ src/main/java/elucent/eidolon/common/item/curio/AngelSightItem.java
@@ -2,9 +2,15 @@ package elucent.eidolon.common.item.curio;
 
 import elucent.eidolon.common.entity.AngelArrowEntity;
 import elucent.eidolon.registries.Registry;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.monster.Enemy;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.ArrowItem;
@@ -16,9 +22,12 @@ import net.minecraft.world.level.Level;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.ArrowLooseEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import org.jetbrains.annotations.NotNull;
 import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.SlotContext;
 
 import java.util.Random;
+import java.util.function.Predicate;
 
 public class AngelSightItem extends EidolonCurio {
     static final Random random = new Random();
@@ -26,6 +35,26 @@ public class AngelSightItem extends EidolonCurio {
     public AngelSightItem(Properties properties) {
         super(properties);
         MinecraftForge.EVENT_BUS.addListener(AngelSightItem::onLoose);
+    }
+
+    @Override
+    public boolean canEquipFromUse(SlotContext slotContext, ItemStack stack) {
+        return !slotContext.entity().isShiftKeyDown();
+    }
+
+    @Override
+    public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level pLevel, @NotNull Player pPlayer, @NotNull InteractionHand pUsedHand) {
+        if (pPlayer.isShiftKeyDown() && !pLevel.isClientSide) {
+            ItemStack stack = pPlayer.getItemInHand(pUsedHand);
+            CompoundTag tag = stack.getOrCreateTag();
+            int mode = tag.getInt("mode");
+            mode = (mode + 1) % 3;
+            tag.putInt("mode", mode);
+            stack.setTag(tag);
+            pPlayer.sendSystemMessage(Component.translatable("eidolon.angels_sight.mode." + mode));
+            return InteractionResultHolder.success(stack);
+        }
+        return super.use(pLevel, pPlayer, pUsedHand);
     }
 
     @SubscribeEvent
@@ -40,17 +69,33 @@ public class AngelSightItem extends EidolonCurio {
             ammo = new ItemStack(Items.ARROW);
         }
 
-        if (CuriosApi.getCuriosHelper().findFirstCurio(event.getEntity(), Registry.ANGELS_SIGHT.get()).isEmpty())
-            return;
+        var ring = CuriosApi.getCuriosHelper().findFirstCurio(event.getEntity(), Registry.ANGELS_SIGHT.get());
+
+        Predicate<LivingEntity> mode = null;
+
+        if (ring.isEmpty()) return;
+        else {
+            CompoundTag ringTag = ring.get().stack().getTag();
+            if (ringTag != null) {
+                mode = switch (ringTag.getInt("mode")) {
+                    case 1 -> (e) -> !(e instanceof Player);
+                    case 2 -> (e) -> e instanceof Enemy;
+                    default -> (e) -> true;
+                };
+            }
+        }
 
         float f = event.getBow().getItem() instanceof BowItem ? BowItem.getPowerForTime(event.getCharge()) : 1.0F;
-        if (!((double)f < 0.1D)) {
+        if (!(f < 0.1F)) {
             boolean flag1 = player.getAbilities().instabuild || (ammo.getItem() instanceof ArrowItem && ((ArrowItem) ammo.getItem()).isInfinite(ammo, stack, player));
             if (!world.isClientSide) {
                 ArrowItem arrowitem = (ArrowItem) (ammo.getItem() instanceof ArrowItem ? ammo.getItem() : Items.ARROW);
                 AbstractArrow innerarrow = arrowitem.createArrow(world, ammo, player);
                 if (stack.getItem() instanceof BowItem bow) innerarrow = bow.customArrow(innerarrow);
                 AngelArrowEntity abstractarrowentity = new AngelArrowEntity(world, player);
+                if (mode != null) {
+                    abstractarrowentity.mode = mode;
+                }
                 abstractarrowentity.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, f * 3.0F, 1.0F);
                 if (f == 1.0F) {
                     innerarrow.setCritArrow(true);
