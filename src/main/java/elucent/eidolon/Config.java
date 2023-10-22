b1ab878f4ba44a5464df20907ac4e153375ba0ad
Alexthw <77152778+Alexthw46@users.noreply.github.com>
2023-10-22 12:58:41 +0200

    Merge pull request #12 from SiverDX/apotheosis_compat
    Apotheosis enchantment compatibility

--- src/main/java/elucent/eidolon/Config.java
+++ src/main/java/elucent/eidolon/Config.java
@@ -7,6 +7,8 @@ import org.apache.commons.lang3.tuple.Pair;
 public class Config {
 	// generic
 	public static ConfigValue<Integer> CRUCIBLE_STEP_DURATION, MAX_ETHEREAL_HEALTH;
+    public static ConfigValue<Integer> SOUL_ENCHANTER_MAXIMUM_USES;
+    public static ConfigValue<Integer> SOUL_ENCHANTER_MAXIMUM_ENCHANTMENTS;
 
     public Config(ForgeConfigSpec.Builder builder) {
         builder.comment("Generic settings").push("generic");
@@ -14,6 +16,10 @@ public class Config {
         		.defineInRange("crucibleStepDuration", 100, 20, 1200);
         MAX_ETHEREAL_HEALTH = builder.comment("Maximum amount of ethereal health (soul half-hearts) an entity can have at once.")
         		.defineInRange("maxEtherealHealth", 80, 0, 1000);
+        SOUL_ENCHANTER_MAXIMUM_USES = builder.comment("How often the Soul Enchanter can apply enchantments on an item (a value below 0 means unlimited)")
+                .define("soulEnchanterMaximumUses", -1);
+        SOUL_ENCHANTER_MAXIMUM_ENCHANTMENTS = builder.comment("How many enchantments the item is allowed to have to be applicable for soul enchanting (a value below 0 means unlimited)")
+                        .define("soulEnchanterMaximumEnchantments", -1);
         builder.pop();
 
     }
