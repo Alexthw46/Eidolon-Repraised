7022583538b2f8945c499d3851195fa1717c0625
Kai <kai.zahn@yahoo.de>
2023-10-24 12:22:51 +0200

    irons spellbooks spell power

--- src/main/java/elucent/eidolon/compat/CompatHandler.java
+++ src/main/java/elucent/eidolon/compat/CompatHandler.java
@@ -2,15 +2,18 @@ package elucent.eidolon.compat;
 
 import elucent.eidolon.Eidolon;
 import elucent.eidolon.compat.apotheosis.Apotheosis;
+import elucent.eidolon.compat.irons_spellbooks.IronsSpellbooks;
 import net.minecraftforge.fml.ModList;
 
 import java.util.Map;
 
 public class CompatHandler {
     public final static String APOTHEOSIS = "apotheosis";
+    public final static String IRONS_SPELLBOOKS = "irons_spellbooks";
 
     private static final Map<String, ModData> MODS = Map.of(
-            APOTHEOSIS, new ModData(Apotheosis::initialize)
+            APOTHEOSIS, new ModData(Apotheosis::initialize),
+            IRONS_SPELLBOOKS, new ModData(IronsSpellbooks::initialize)
     );
 
     public static void initialize() {
