a07d9e52c8e1260bb556cbf14bd636703603b3dc
Alexthw46 <77152778+Alexthw46@users.noreply.github.com>
2023-11-04 11:42:15 +0100

    Fix git being a troll

--- src/main/java/elucent/eidolon/registries/EidolonAttributes.java
+++ src/main/java/elucent/eidolon/registries/EidolonAttributes.java
@@ -1,10 +1,13 @@
 package elucent.eidolon.registries;
 
 import elucent.eidolon.Eidolon;
-import net.minecraft.world.entity.Entity;
+import elucent.eidolon.common.entity.*;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.attributes.RangedAttribute;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.event.entity.EntityAttributeModificationEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -14,28 +17,36 @@ import net.minecraftforge.registries.RegistryObject;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class EidolonAttributes {
-    public static final DeferredRegister<Attribute> ATTRIBUTES = DeferredRegister.create(ForgeRegistries.ATTRIBUTES, Eidolon.MODID);
 
+    static final DeferredRegister<Attribute> ATTRIBUTES = DeferredRegister.create(ForgeRegistries.ATTRIBUTES, Eidolon.MODID);
+    public static final RegistryObject<Attribute> CHANTING_SPEED = ATTRIBUTES.register("chanting_speed", () -> new RangedAttribute(Eidolon.MODID + ".chanting_speed", 1, 0, 100).setSyncable(true));
+    public static final RegistryObject<Attribute> PERSISTENT_SOUL_HEARTS = ATTRIBUTES.register("persistent_soul_hearts", () -> new RangedAttribute(Eidolon.MODID + ".persistent_soul_hearts", 0, 0, 2000).setSyncable(true));
+    public static final RegistryObject<Attribute>
+            MAX_SOUL_HEARTS = ATTRIBUTES.register("max_soul_hearts", () -> new RangedAttribute(Eidolon.MODID + ".max_soul_hearts", 80, 0, 2000).setSyncable(true));
     public static final RegistryObject<Attribute> MAGICAL_KNOWLEDGE = ATTRIBUTES.register("magical_knowledge", () -> new RangedAttribute("attribute.eidolon.magical_knowledge", 1, 0, 10).setSyncable(true));
 
-    public static float getMagicalKnowledge(final Entity entity) {
-        if (!(entity instanceof LivingEntity livingEntity)) {
-            return 1;
+    @SubscribeEvent
+    public static void addCustomAttributes(EntityAttributeModificationEvent event) {
+        for (EntityType<? extends LivingEntity> t : event.getTypes()) {
+            if (event.has(t, Attributes.MAX_HEALTH)) {
+                event.add(t, PERSISTENT_SOUL_HEARTS.get());
+                event.add(t, MAX_SOUL_HEARTS.get());
+            }
+            if (t == EntityType.PLAYER) {
+                event.add(t, MAGICAL_KNOWLEDGE.get());
+                event.add(t, CHANTING_SPEED.get());
+            }
         }
-
-        return (float) livingEntity.getAttributeValue(MAGICAL_KNOWLEDGE.get());
-    }
-
-    public static float getSpellDamage(final Entity entity, float baseValue) {
-        return baseValue * getMagicalKnowledge(entity);
-    }
-
-    public static int getSpellCooldown(final Entity entity, float baseValue) {
-        return (int) (baseValue / getMagicalKnowledge(entity));
     }
 
     @SubscribeEvent
-    public static void setAttributes(final EntityAttributeModificationEvent event) {
-        event.getTypes().forEach(type -> event.add(type, MAGICAL_KNOWLEDGE.get()));
+    public static void defineAttributes(EntityAttributeCreationEvent event) {
+        event.put(EidolonEntities.ZOMBIE_BRUTE.get(), ZombieBruteEntity.createAttributes());
+        event.put(EidolonEntities.WRAITH.get(), WraithEntity.createAttributes());
+        event.put(EidolonEntities.NECROMANCER.get(), NecromancerEntity.createAttributes());
+        event.put(EidolonEntities.RAVEN.get(), RavenEntity.createAttributes());
+        event.put(EidolonEntities.SLIMY_SLUG.get(), SlimySlugEntity.createAttributes());
+        event.put(EidolonEntities.GIANT_SKEL.get(), GiantSkeletonEntity.createAttributes().build());
     }
+
 }
