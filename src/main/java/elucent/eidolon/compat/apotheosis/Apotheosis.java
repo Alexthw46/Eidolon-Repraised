93993f44cb658dc9ab516e3d9fd914aeeaea88db
Alexthw46 <77152778+Alexthw46@users.noreply.github.com>
2023-11-04 18:51:21 +0100

    Fix Apoth version conflicts - 3

--- src/main/java/elucent/eidolon/compat/apotheosis/Apotheosis.java
+++ src/main/java/elucent/eidolon/compat/apotheosis/Apotheosis.java
@@ -9,18 +9,21 @@ import dev.shadowsoffire.apotheosis.adventure.loot.LootCategory;
 import dev.shadowsoffire.apotheosis.adventure.loot.LootRarity;
 import dev.shadowsoffire.apotheosis.ench.asm.EnchHooks;
 import dev.shadowsoffire.placebo.reload.DynamicHolder;
+import dev.shadowsoffire.placebo.util.StepFunction;
 import elucent.eidolon.Eidolon;
 import elucent.eidolon.common.item.WandItem;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
 
 
 public class Apotheosis {
-    public static final LootCategory WAND = LootCategory.register(LootCategory.SWORD, "wand", itemStack -> itemStack.getItem() instanceof WandItem, toArray(EquipmentSlot.MAINHAND));
+    private static final EquipmentSlot[] wandSlots = new EquipmentSlot[]{EquipmentSlot.MAINHAND, EquipmentSlot.OFFHAND};
+    public static final LootCategory WAND = LootCategory.register(LootCategory.SWORD, "wand", itemStack -> itemStack.getItem() instanceof WandItem, wandSlots);
 
     public static boolean isTreasureOnly(final Enchantment enchantment) {
         return EnchHooks.isTreasureOnly(enchantment);
@@ -30,18 +33,11 @@ public class Apotheosis {
         return EnchHooks.getMaxLevel(enchantment);
     }
 
-    public static int rarityToNumber(final LootRarity rarity) {
-        return rarity.ordinal();
-    }
-
-    public static int affixToAmount(final LootRarity affixRarity, final LootRarity minRarity) {
-        // This way the amount starts at the minimum rarity with 1
-        return 1 + Apotheosis.rarityToNumber(affixRarity) - Apotheosis.rarityToNumber(minRarity);
-    }
-
-
-    private static EquipmentSlot[] toArray(final EquipmentSlot... equipmentSlots) {
-        return equipmentSlots;
+    public static int affixToAmount(final AffixInstance affixInstance) {
+        if (affixInstance.affix().get() instanceof StepScalingAffix scalingAffix) {
+            return (int) scalingAffix.affixToAmount(affixInstance);
+        }
+        return 0;
     }
 
     public static void initialize() {
@@ -58,13 +54,28 @@ public class Apotheosis {
         for (DynamicHolder<? extends Affix> affix : affixes.keySet()) {
             AffixInstance affixInstance = affixes.get(affix);
 
-            if (affix.get() instanceof HailingAffix hailingAffix) {
-                projectileAmount += affixToAmount(affixInstance.rarity().get(), hailingAffix.getMinRarity());
-            } else if (affix.get() instanceof TrackingAffix trackingAffix) {
-                trackingAmount += affixToAmount(affixInstance.rarity().get(), trackingAffix.getMinRarity());
+            if (affix.get() instanceof HailingAffix) {
+                projectileAmount += affixToAmount(affixInstance);
+            } else if (affix.get() instanceof TrackingAffix) {
+                trackingAmount += affixToAmount(affixInstance);
             }
         }
 
         return Pair.of(projectileAmount, trackingAmount);
     }
+
+    public interface StepScalingAffix {
+
+        @NotNull Map<LootRarity, StepFunction> getValues();
+
+        default float affixToAmount(LootRarity rarity, float level) {
+            return getValues().get(rarity).get(level);
+
+        }
+
+        default float affixToAmount(final AffixInstance affixInstance) {
+            return affixToAmount(affixInstance.rarity().get(), affixInstance.level());
+        }
+    }
+
 }
--- src/main/java/elucent/eidolon/compat/apotheosis/HailingAffix.java
+++ src/main/java/elucent/eidolon/compat/apotheosis/HailingAffix.java
@@ -4,46 +4,47 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import dev.shadowsoffire.apotheosis.adventure.affix.Affix;
 import dev.shadowsoffire.apotheosis.adventure.affix.AffixType;
+import dev.shadowsoffire.apotheosis.adventure.affix.socket.gem.bonus.GemBonus;
 import dev.shadowsoffire.apotheosis.adventure.loot.LootCategory;
 import dev.shadowsoffire.apotheosis.adventure.loot.LootRarity;
 import dev.shadowsoffire.placebo.json.PSerializer;
+import dev.shadowsoffire.placebo.util.StepFunction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.Map;
 import java.util.function.Consumer;
 
-public class HailingAffix extends Affix {
-    public static final Codec<HailingAffix> CODEC = RecordCodecBuilder.create(inst -> inst
-            .group(
-                    LootRarity.CODEC.fieldOf("min_rarity").forGetter(HailingAffix::getMinRarity))
-            .apply(inst, HailingAffix::new));
+public class HailingAffix extends Affix implements Apotheosis.StepScalingAffix {
+    public static final Codec<HailingAffix> CODEC = RecordCodecBuilder.create(inst -> inst.group(GemBonus.VALUES_CODEC.fieldOf("values").forGetter((a) -> a.values)).apply(inst, HailingAffix::new));
+    protected final Map<LootRarity, StepFunction> values;
 
 
     public static final PSerializer<HailingAffix> SERIALIZER = PSerializer.fromCodec("Hailing Affix", CODEC);
 
-    private final LootRarity minRarity;
-
-    public HailingAffix(final LootRarity minRarity) {
+    public HailingAffix(Map<LootRarity, StepFunction> values) {
         super(AffixType.ABILITY);
-        this.minRarity = minRarity;
-    }
-
-    public LootRarity getMinRarity() {
-        return minRarity;
+        this.values = values;
     }
 
     @Override
     public boolean canApplyTo(final ItemStack stack, final LootCategory category, final LootRarity rarity) {
-        return category == Apotheosis.WAND && rarity.isAtLeast(this.minRarity);
+        return category == Apotheosis.WAND && this.values.containsKey(rarity);
     }
 
     @Override
     public void addInformation(final ItemStack stack, final LootRarity rarity, float level, final Consumer<Component> list) {
-        list.accept(Component.translatable("affix." + this.getId() + ".desc", Apotheosis.affixToAmount(rarity, getMinRarity())));
+        list.accept(Component.translatable("affix." + this.getId() + ".desc", fmt(affixToAmount(rarity, level))));
     }
 
     @Override
     public PSerializer<? extends Affix> getSerializer() {
         return SERIALIZER;
+    }
+
+    @Override
+    public @NotNull Map<LootRarity, StepFunction> getValues() {
+        return values;
     }
 }
