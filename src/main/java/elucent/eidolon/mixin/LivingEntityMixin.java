a4a3b53b942f5d0c3e205d79fa7e95bbc8fb36f2
Alexthw46 <77152778+Alexthw46@users.noreply.github.com>
2023-11-24 00:00:07 +0100

    Zoglin crash

--- src/main/java/elucent/eidolon/mixin/LivingEntityMixin.java
+++ src/main/java/elucent/eidolon/mixin/LivingEntityMixin.java
@@ -3,6 +3,8 @@ package elucent.eidolon.mixin;
 import elucent.eidolon.Eidolon;
 import elucent.eidolon.capability.IPlayerData;
 import elucent.eidolon.registries.EidolonPotions;
+import elucent.eidolon.util.EntityUtil;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MobType;
@@ -13,9 +15,29 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Optional;
+import java.util.UUID;
+
+import static elucent.eidolon.util.EntityUtil.THRALL_KEY;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
+
+    @Inject(method = "canAttack(Lnet/minecraft/world/entity/LivingEntity;)Z", at = @At("TAIL"), cancellable = true)
+    public void eidolonrepraised$canAttack(LivingEntity pTarget, CallbackInfoReturnable<Boolean> cir) {
+        if (((Entity) (Object) this) instanceof LivingEntity living && pTarget != null) {
+            if (EntityUtil.isEnthralled(living)) {
+                if (EntityUtil.isEnthralledBy(living, pTarget)) cir.setReturnValue(false);
+                UUID master = living.getPersistentData().getUUID(THRALL_KEY);
+                if (living.level().getPlayerByUUID(master) instanceof ServerPlayer player) {
+                    LivingEntity lastHurt = player.getLastHurtMob();
+                    LivingEntity lastHurtBy = player.getLastHurtByMob();
+                    // if the target is not one of the player's last hurt mobs, don't attack
+                    if (lastHurt != pTarget && lastHurtBy != pTarget) cir.setReturnValue(false);
+                }
+            }
+        }
+    }
+
     @Inject(method = "getMobType", at = @At("HEAD"), cancellable = true)
     public void eidolonrepraised$getMobType(CallbackInfoReturnable<MobType> ci) {
         //doublecast to not make the IDE freak out, activeEffectsMaps can be not initialized for reasons
@@ -26,9 +48,9 @@ public class LivingEntityMixin {
 
     @Inject(method = "isFallFlying", at = @At("HEAD"), cancellable = true)
     public void eidolonrepraised$isFallFlying(CallbackInfoReturnable<Boolean> ci) {
-       if ((LivingEntity)(Object)this instanceof Player p) {
-    	   Optional<IPlayerData> opt = p.getCapability(IPlayerData.INSTANCE).resolve();
-    	   if (opt.isPresent() && opt.get().isDashing(p)) ci.setReturnValue(true);
-       }
+        if ((LivingEntity) (Object) this instanceof Player p) {
+            Optional<IPlayerData> opt = p.getCapability(IPlayerData.INSTANCE).resolve();
+            if (opt.isPresent() && opt.get().isDashing(p)) ci.setReturnValue(true);
+        }
     }
 }
