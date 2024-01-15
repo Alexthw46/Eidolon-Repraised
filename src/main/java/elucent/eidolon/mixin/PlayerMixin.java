36169475e071e3ce9829caa53e90e943a0512b34
Alexthw46 <77152778+Alexthw46@users.noreply.github.com>
2024-01-15 11:55:43 +0100

    fix athame chances, fix slugs not being really tamable, change raven dismounting, increase raven spawn rate

---
+++ src/main/java/elucent/eidolon/mixin/PlayerMixin.java
@@ -0,0 +1,37 @@
+package elucent.eidolon.mixin;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Player.class)
+public abstract class PlayerMixin {
+
+    @Shadow
+    public abstract CompoundTag getShoulderEntityLeft();
+
+    @Shadow
+    public abstract CompoundTag getShoulderEntityRight();
+
+    @Inject(at = @At("HEAD"), method = "removeEntitiesOnShoulder", cancellable = true)
+    void eidolonrepraised$removeEntitiesOnShoulder(CallbackInfo ci) {
+        if ((Object) this instanceof Player player) {
+            CompoundTag leftShoulder = getShoulderEntityLeft(), rightShoulder = getShoulderEntityRight();
+            if (leftShoulder != null) {
+                if (leftShoulder.getString("id").equals("eidolon:raven") && player.isShiftKeyDown()) {
+                    ci.cancel();
+                }
+            }
+            if (rightShoulder != null) {
+                if (rightShoulder.getString("id").equals("eidolon:raven") && player.isShiftKeyDown()) {
+                    ci.cancel();
+                }
+            }
+        }
+    }
+
+}
