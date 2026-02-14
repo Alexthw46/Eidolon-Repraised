da483bcc376661fa624487f602bdf450f68aa787
Alexthw <77152778+Alexthw46@users.noreply.github.com>
2026-02-14 17:56:30 +0100

    avoid pathfind recalc each tick on the goal-based rituals

--- src/main/java/alexthw/eidolon_repraised/common/entity/ai/GoToPositionGoal.java
+++ src/main/java/alexthw/eidolon_repraised/common/entity/ai/GoToPositionGoal.java
@@ -4,10 +4,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.ai.goal.Goal;
 
-import java.util.Random;
-
 public class GoToPositionGoal extends Goal {
-    Random random = new Random();
     final BlockPos dest;
     final PathfinderMob creature;
     final double speed;
@@ -21,10 +18,22 @@ public class GoToPositionGoal extends Goal {
     }
 
     @Override
+    public void start() {
+        super.start();
+        creature.getNavigation().moveTo(dest.getX(), dest.getY(), dest.getZ(), speed);
+    }
+
+    @Override
     public void tick() {
-        if (running) {
-            creature.getNavigation().moveTo(dest.getX(), dest.getY(), dest.getZ(), speed);
-            if (creature.distanceToSqr(dest.getX(), dest.getY(), dest.getZ()) < 8 * 8) running = false;
+        if (running && creature.level().getGameTime() % 20 == 0) {
+            if (creature.getTarget() != null) {
+                creature.setTarget(null);
+            }
+            if (creature.distanceToSqr(dest.getX(), dest.getY(), dest.getZ()) < 8 * 8) {
+                running = false;
+            } else if (creature.getNavigation().isDone()) {
+                creature.getNavigation().moveTo(dest.getX(), dest.getY(), dest.getZ(), speed);
+            }
         }
     }
 
